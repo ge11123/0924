18e14f2654cdc95597ed8cddee6dffa2c34e2df0
ge123 <as8354093@gmail.com>
2022-09-24 13:24:47 +0800

    first

---
+++ WebApplication1/Controllers/ReadExcelController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReadExcelController : ControllerBase
+    {
+    
+        private readonly ILogger<ReadExcelController> _logger;
+
+        public ReadExcelController(ILogger<ReadExcelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "ExcelRead")]
+        public string Get()
+        {
+            string filePath = @"C:\Users\as835\Documents\csharp\monitor\ALC.xlsx";
+            Microsoft.Office.Interop.Excel.Application excel = new();
+            Workbook wb;
+            Worksheet ws;
+
+            wb = excel.Workbooks.Open(filePath);
+            ws = (Worksheet)wb.Worksheets[0];
+
+            return ws.Name;
+            
+        }
+    }
+}
